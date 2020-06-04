6213a72ddd87c1a82b249ff850e68154029fe4a9
Denis Avdeev <davdeev@me.com>
2020-06-04 08:20:42 +0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Denis Avdeev on 04.06.2020.
+//  Copyright © 2020 Denis Avdeev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
